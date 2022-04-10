5e63c0969e3f63b11d1214b77e31ad7bcc17373f
Denis Demko <denis16085@gmail.com>
2022-04-10 14:20:50 +0300

    Added day1 solution

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int i = 1;
+        while (i <= 10) {
+            System.out.print("JAVA ");
+            i += 1;
+        }
     }
 }
