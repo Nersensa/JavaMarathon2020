e6dd37d43dd300deafa2ec7cce83231c7eb1fb96
Denis Demko <denis16085@gmail.com>
2022-04-12 14:34:06 +0300

    Added daay3 solution

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 5;
+        while (count > 0) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
 
+            if (b == 0d) {
+                System.out.println("Деление на 0");
+            }
+
+            if (b != 0d) {
+                System.out.println(a / b);
+            }
+            count -= 1;
+        }
     }
 }
